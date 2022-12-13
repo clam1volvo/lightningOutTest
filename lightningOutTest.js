6ab48f48b0e6c8e6e639e5da68c525791399c264
clam1volvo <111749247+clam1volvo@users.noreply.github.com>
2022-12-13 13:29:44 +0100

    Delete lightningOutTest.js

--- lightningOutTest.js
+++
@@ -1,7 +0,0 @@
-
-import {LightningElement} from 'lwc';
-//import updateObjects from '@salesforce/apex/optOutFormController.updateSObjects';
-
-export default class lightningOutTest extends LightningElement {
-    
-}
